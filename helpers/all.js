a63b3372a0f6385f199f73af08c2c987a8fdb8db
diego <diego.sanches@gmail.com>
2015-02-05 10:16:06 -0300

    Style updated

--- helpers/all.js
+++ helpers/all.js
@@ -4,10 +4,25 @@
 var util = require('util');
 var path = require('path');
 
+/***
+* It returns 'style_even' whether index is even or 'style_odd' whether
+* index is odd
+*/
+exports.alternateClass = function (index, style) {
+	return style + ( (index % 2) ? '_odd':'_even' );
+};
+
+/****
+* Returns file relative to current directory. It avoids showing
+* full path file
+*/
 exports.relativePath = function (file, current) {
 	return path.relative(current, file); 
 };
 
+/***
+* It composses resource depending on file type
+**/
 exports.resourcePath = function  (entry, currentDirectory) {
 	var format = (entry.status.isDirectory() ) ? '/directory' : '/file';
 
